bc907501b3dfd07c469323529697bc8d37c75594
Akram Machkour <126728825+machkourakram1@users.noreply.github.com>
2024-12-17 14:20:28 +0100

    Add files via upload

---
+++ Scripts/script.js
@@ -0,0 +1,8 @@
+// Typing Animation
+
+var typed = new Typed(".typing", {
+    strings:["", "Web Designer", "Web Developer", "Graphic Designer", "Full Stack Web Developer"], 
+    typeSpeed:100, 
+    BackSpeed:60,
+    loop:true
+})
